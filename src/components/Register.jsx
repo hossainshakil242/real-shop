4966b5ec7536dc1e6949e7bf46a6423a07651c6f
hossainshakil242 <realshakil8@gmail.com>
2023-08-04 15:44:45 +0600

    createAccoutnWithGoogleAuth (done)

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,8 +8,16 @@ const emailVerificationAlert = () => toast('Please verification your email.');
 const emailAlreadyUsed = () => toast('This Email Already Used.');
 
 const Register = () => {
-    const{user,createUser,sendVerificationEmail,logOut} = useContext(AuthContext);
+    const { user, createUser, sendVerificationEmail, createAccoutnWithGoole, logOut } = useContext(AuthContext);
     // console.log(createUser);
+
+    const handleWithGoogle = () => {
+        createAccoutnWithGoole()
+            .then(result => {
+                const WithGoogleLogged = result.user;
+                console.log(WithGoogleLogged);
+            })
+    }
 
     const handleRegister = (e) => {
         e.preventDefault();
@@ -17,77 +25,92 @@ const Register = () => {
         const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(name,email, password);
+        console.log(name, email, password);
 
-        createUser(email,password)
-        .then(result =>{
-            const loggedUser = result.user;
-            console.log(loggedUser);
-
-            sendVerificationEmail(result.user)
-            .then(result=>{
+        createUser(email, password)
+            .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                if(!loggedUser.emailVerified){
-                    logOut()
-                    .then(()=>{
-                        console.log('logout');
+
+                sendVerificationEmail(result.user)
+                    .then(result => {
+                        const loggedUser = result.user;
+                        console.log(loggedUser);
+                        if (!loggedUser.emailVerified) {
+                            logOut()
+                                .then(() => {
+                                    console.log('logout');
+                                })
+                                .then(error => {
+                                    console.log(error);
+                                })
+                        }
+                        form.reset();
                     })
-                    .then(error=>{
+                    .catch(error => {
                         console.log(error);
+                        emailVerificationAlert();
                     })
-                }
-                form.reset();
             })
-            .catch(error=>{
-                console.log(error);
-                emailVerificationAlert();
+            .catch(error => {
+                const alreadyUse = `Firebase: Error (auth/email-already-in-use).`;
+                error.message === alreadyUse && emailAlreadyUsed();
+                console.log(error.message);
+
             })
-        })
-        .catch(error=>{
-            const alreadyUse = `Firebase: Error (auth/email-already-in-use).`;
-            error.message === alreadyUse  && emailAlreadyUsed();
-            console.log(error.message);
-            
-        })
     }
+
 
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Register Here!</h1>
+                <div className="hero-content flex-row-reverse gap-20">
+                    {/* Signup With other option */}
+                    <div>
+                        <h1 className="text-3xl text-center pb-5 font-bold">Easy SignUp</h1>
+                        <div className='space-x-2'>
+
+                            <button onClick={handleWithGoogle} className=" btn btn-outline btn-primary">Login With Google</button>
+
+                            <button className=" btn btn-outline btn-primary">Login With Github</button>
+
+                        </div>
                     </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <form onSubmit={handleRegister} className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Full Name</span>
-                                </label>
-                                <input type="text" name='name' placeholder="jhon smith" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="email" name='email' placeholder="jhon@gmail.com" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Register</button>
-                            </div>
 
-                            <div className=" text-center">
-                                <Link to='/login'><a href="#">Already Have an Account?</a></Link>
-                            </div>
-                        </form>
+                    <div>
+                        <div className="text-center lg:text-left ">
+                            <h1 className="text-5xl pb-5 font-bold">Register Here!</h1>
+                        </div>
+                        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                            <form onSubmit={handleRegister} className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Full Name</span>
+                                    </label>
+                                    <input type="text" name='name' placeholder="jhon smith" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name='email' placeholder="jhon@gmail.com" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Register</button>
+                                </div>
 
+                                <div className=" text-center">
+                                    <Link to='/login'><a href="#">Already Have an Account?</a></Link>
+                                </div>
+                            </form>
+
+                        </div>
                     </div>
                 </div>
             </div>
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,15 +1,20 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+
+    const createAccoutnWithGoole = () => {
+        return signInWithPopup(auth, googleProvider)
+    }
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
@@ -61,6 +66,7 @@ const AuthProvider = ({ children }) => {
         createUser,
         sendVerificationEmail,
         signIn,
+        createAccoutnWithGoole,
         logOut
     }
 
